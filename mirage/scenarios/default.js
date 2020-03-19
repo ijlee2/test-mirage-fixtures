ed88875ecdd3a6f57b54e2b6929f3111c160dee8
ijlee2 <ijlee2@users.noreply.github.com>
2020-03-19 08:22:59 -0500

    Added the server.create approach to show that it does continue to work

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -1,3 +1,103 @@
 export default function(server) {
     server.loadFixtures();
+
+
+    /* Albums created this way don't appear in tests as expected
+
+    server.create('album', {
+        name: 'Pure Heroine',
+        publicationDate: '2013',
+        tracks: [
+            server.create('track', {
+                name: 'Tennis Court',
+            }),
+
+            server.create('track', {
+                name: '400 Lux',
+            }),
+
+            server.create('track', {
+                name: 'Royals',
+            }),
+
+            server.create('track', {
+                name: 'Ribs',
+            }),
+
+            server.create('track', {
+                name: 'Buzzcut Season',
+            }),
+
+            server.create('track', {
+                name: 'Team',
+            }),
+
+            server.create('track', {
+                name: 'Glory and Gore',
+            }),
+
+            server.create('track', {
+                name: 'Still Sane',
+            }),
+
+            server.create('track', {
+                name: 'White Teeth Teens',
+            }),
+
+            server.create('track', {
+                name: 'A World Alone',
+            }),
+        ]
+    });
+
+    server.create('album', {
+        name: 'Melodrama',
+        publicationDate: '2017',
+        tracks: [
+            server.create('track', {
+                name: 'Green Light',
+            }),
+
+            server.create('track', {
+                name: 'Sober',
+            }),
+
+            server.create('track', {
+                name: 'Homemade Dynamite',
+            }),
+
+            server.create('track', {
+                name: 'The Louvre',
+            }),
+
+            server.create('track', {
+                name: 'Liability',
+            }),
+
+            server.create('track', {
+                name: 'Hard Feelings/Loveless',
+            }),
+
+            server.create('track', {
+                name: 'Sober II (Melodrama)',
+            }),
+
+            server.create('track', {
+                name: 'Writer in the Dark',
+            }),
+
+            server.create('track', {
+                name: 'Supercut',
+            }),
+
+            server.create('track', {
+                name: 'Liability (Reprise)',
+            }),
+
+            server.create('track', {
+                name: 'Perfect Places',
+            }),
+        ]
+    });
+    */
 }
